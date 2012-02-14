b3e204d06d780173b28c817dd073b65f8a279cab
ernie <ernie@epicbytes.net>
2012-02-14 11:24:41 +0100

    bvl was wrong

--- project1/main.py
+++ project1/main.py
@@ -360,7 +360,7 @@ if __name__ == '__main__':
 
         #Set bit vector length
         if o in ('--bvl'):
-            generations = int(a)
+            bvl = int(a)
 
         #Set number of children that are allowed to grow up
         if o in ('--fitchildren'):
