5bff8d0a2096f5d8699c3e167554bcc873b6d790
ernie <ernie@epicbytes.net>
2012-02-24 13:06:55 +0100

    copied project1 to project2 in preparation for project2 which will build on project1

---
+++ project2/main.py
@@ -0,0 +1,238 @@
+from __future__ import division
+from math import sqrt
+import getopt
+import sys
+from random import randint, random, shuffle
+
+import matplotlib.pyplot as plt
+import adultselection as adsel
+import genotype as geno
+import parentselection as parsel
+import phenotype as pheno
+
+def average(values):
+    return sum(values, 0.0) / len(values)
+
+def standard_deviation(values):
+    avg = average(values)
+    tmp = 0
+    for f in values:
+        tmp += (f-avg)**2
+    tmp /= len(values)
+    return sqrt(tmp)
+
+def get_adult_selection(adultselect, population, fitchildren):
+    if adultselect == "A_I":
+        return adsel.A_I(population)
+    elif adultselect == "A_II":
+        return adsel.A_II(population, fitchildren)
+    elif adultselect == "A_III":
+        return adsel.A_III(population, fitchildren)
+
+def get_parent_selection(parentselect, k, eps, max_ft, min_ft):
+    if parentselect == "P_I":
+        return parsel.NormalizedRoulette
+    elif parentselect == "P_II":
+        return parsel.SigmaScaling
+    elif parentselect == "P_III":
+        return parsel.TournamentSelectionFactory(k, eps)
+    elif parentselect == "P_IV":
+        return parsel.RankSelectionFactory(max_ft, min_ft)
+
+class EA(object):
+
+    def __init__(self, population, crossover,
+            mutation, genotype, adult_selection,
+            parent_selection, figure, **kwargs):
+        self.individuals = []
+        for i in xrange(population):
+            self.individuals.append(genotype(None, **kwargs))
+        self.crossover = crossover
+        self.mutation = mutation
+        self.adult_selection = adult_selection
+        self.parent_selection = parent_selection
+        self.population = population
+        self.pts = []
+        self.genotype = genotype
+        self.figure = figure
+
+    def loop(self, generations):
+        def reproduce(p1,p2):
+            cr = random()
+            cg = None
+            if cr < self.crossover:
+                cg = p1.genotype.crossover(p2.genotype) 
+            else:
+                cg = p1.genotype
+            cr = random()
+            if cr < self.mutation:
+                cg.mutate()
+            return cg
+        maxs = []
+        avgs = []
+        sds = []  # standard deviations
+        strategy_entropies = []
+
+        if hasattr(self.individuals[0].phenotype, 'strategy_entropy'):
+            doentropy = True
+        else:
+            doentropy = False
+
+        for i in xrange(generations):
+            self.pts = [x.develop(self) for x in self.individuals]
+            fitness = [x.fitness for x in self.pts]
+            maxs.append(max(fitness))
+            sds.append(standard_deviation(fitness))
+            print "Winner",i,max(self.pts, key=lambda x: x.fitness), maxs[-1], len(self.pts)
+            avgs.append(average(fitness))
+            if doentropy:
+                strategy_entropies.append(average([x.strategy_entropy for x in self.pts]))
+
+
+            adults = self.adult_selection.select(self.pts)
+            retain = self.adult_selection.retain(adults)
+
+            children = []
+            i = self.adult_selection.produce()
+            for p1,p2 in self.parent_selection(adults):
+                ## reproduction
+                children.append(reproduce(p1,p2))
+                i -= 1
+                if i <= 0: break
+                children.append(reproduce(p2,p1))
+                i -= 1
+                if i <= 0: break
+            
+            self.individuals = children + [x.genotype for x in retain]
+            
+        ext = ".png"
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(maxs)
+        ax.plot(sds)
+        ax.plot(avgs)
+        fig.savefig(self.figure+ext)
+
+        if strategy_entropies:
+            fig2 = plt.figure()
+            ax = fig2.add_subplot(111)
+            ax.plot(strategy_entropies)
+            fig2.savefig(self.figure+'_entropies'+ext)
+
+        print max(maxs), maxs.index(max(maxs))
+        #p.plot(maxs)
+        #p.plot(avgs)
+        #p.save("figure.png")
+
+if __name__ == '__main__':
+    #Default values
+    population = 30
+    generations = 100
+    bvl = 40
+    fitchildren = 40
+    adultselect = "A_I"
+    parentselect = "P_I"
+    phenotype = pheno.OneMaxPhenotype
+    k = 20
+    eps = 0.05
+    max_ft = 1.5
+    min_ft = 0.5
+    crossover = 0.9
+    mutation = 0.1
+    figure = "figure"
+    Rf = 0.5
+    Lf = 0.5
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "",
+                ['population=',
+                    'generations=',
+                    'bvl=',
+                    'fitchildren=',
+                    'adultselect=',
+                    'parentselect=',
+                    'min=',
+                    'max=', 
+                    'k=', 
+                    'e=',
+                    'phenotype=',
+                    'crossover=',
+                    'mutation=',
+                    'figure=',
+                    'rf=',
+                    'lf='
+                    ])
+    except getopt.GetoptError:
+        print "Available options: --population, --generations, --bvl, --fitchildren, --adultselect, --parentselect, --k, --e, --min, --max, --phenotype, --crossover, --mutation, --figure"
+        sys.exit()
+    
+    for o, a in opts:
+        #Set population size
+        if o in ('--population'):
+            population = int(a)
+
+        #Set number of generations
+        if o in ('--generations'):
+            generations = int(a)
+
+        #Set bit vector length
+        if o in ('--bvl'):
+            bvl = int(a)
+
+        #Set number of children that are allowed to grow up
+        if o in ('--fitchildren'):
+            fitchildren = int(a)
+
+        #Set the adult selection mechanism
+        if o in ('--adultselect'):
+            adultselect = str(a)
+
+        #Set the parent selection mechanism
+        if o in ('--parentselect'):
+            parentselect = str(a)
+
+        if o in ('--k'):
+            k = int(a)
+
+        if o in ('--e'):
+            eps = float(a)
+
+        if o in ('--min'):
+            min_ft = float(a)
+
+        if o in ('--max'):
+            max_ft = float(a)
+
+        if o in ('--phenotype'):
+            try:
+                phenotype = eval("pheno."+a)
+            except:
+                print "You don't exist. Go away!"
+                sys.exit(-1)
+
+        if o in ('--crossover'):
+            crossover = float(a)
+
+        if o in ('--mutation'):
+            mutation = float(a)
+
+        if o in ('--figure'):
+            figure = str(a)
+
+        if o in ('--rf'):
+            Rf = float(a)
+
+        if o in ('--lf'):
+            Lf = float(a)
+
+    ea = EA(fitchildren, crossover, mutation,
+            geno.BitVectorGenotype,
+            figure=figure,
+            length=bvl,
+            Rf=Rf,
+            Lf=Lf,
+            adult_selection=get_adult_selection(adultselect, population, fitchildren),
+            parent_selection=get_parent_selection(parentselect, k, eps, max_ft, min_ft),
+            phenotype=phenotype)
+    ea.loop(generations)
---
+++ project2/phenotype.py
@@ -0,0 +1,128 @@
+from __future__ import division
+from abc import ABCMeta, abstractmethod, abstractproperty
+import main
+import genotype as geno
+import math
+
+from random import randint, random, shuffle
+
+def average(values):
+    return sum(values, 0.0) / len(values)
+
+def simulate_war(this, others, Rf, Lf):
+    wins = 0
+    length = len(this.strategy)
+    for os in others:
+        if os == this: continue
+        iwin = 0
+        assert len(this.strategy) == len(os.strategy)
+        mytroops = [float(x) for x in this.strategy]
+        ostroops = [float(x) for x in os.strategy]
+        my_sf = 1.0 # strength factor
+        os_sf = 1.0
+        for i in range(len(mytroops)):
+            if my_sf * mytroops[i] > os_sf * ostroops[i]:
+                reploy = Rf * (mytroops[i] - ostroops[i]) / (length - i)
+                for j in range(i, len(mytroops)):
+                    mytroops[j] += reploy
+                os_sf -= Lf
+                iwin += 1
+            elif my_sf * mytroops[i] < os_sf * ostroops[i]:
+                reploy = Rf * (ostroops[i] - mytroops[i]) / (length - i)
+                for j in range(i, len(mytroops)):
+                    ostroops[j] += reploy
+                my_sf -= Lf
+                iwin -= 1
+        if iwin > 0:
+            wins += 2
+        elif iwin == 0:
+            wins += 1 
+    return wins
+
+class Phenotype(object):
+    __metaclass__ = ABCMeta
+
+    @abstractproperty
+    def fitness(self): pass
+
+
+class OneMaxPhenotype(Phenotype):
+
+    def __init__(self, genotype, generation):
+        assert isinstance(genotype, geno.BitVectorGenotype)
+        self.genotype = genotype
+        self.generation = generation
+
+    @property
+    def fitness(self):
+        return average(self.genotype.vector)
+
+    def __repr__(self):
+        s = "<OneMaxPhenotype("
+        s += "".join(str(x) for x in self.genotype.vector)
+        s += ")"
+        return s
+
+class NeedlePhenotype(Phenotype):
+    #sol = [1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,0,0,1,1,1,1,0,1,0,1,0,0,1,1,1,0] 
+    sol = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
+
+    def __init__(self, genotype, generation):
+        assert isinstance(genotype, geno.BitVectorGenotype)
+        assert len(genotype.vector) == len(self.sol)
+        self.genotype = genotype
+        self.generation = generation
+
+    @property
+    def fitness(self):
+        f = 0
+        for a,b in zip(self.sol,self.genotype.vector):
+            if a==b:
+                f += 1
+        return f
+
+    def __repr__(self):
+        s = "<NeedlePhenotype("
+        s += "".join(str(x) for x in self.genotype.vector)
+        s += ")"
+        return s
+
+
+class BlottoPhenotype(Phenotype):
+
+    def __init__(self, genotype, ea):
+        #print genotype, isinstance(genotype, main.BitVectorGenotype)
+        #assert isinstance(genotype, BitVectorGenotype)
+        self.strategy = []
+        self.genotype = genotype
+        for i in range(0, len(genotype.vector), 10):
+            self.strategy.append(sum(genotype.vector[i:i+10]))
+        tmsum = sum(self.strategy,0.0)
+        for i in range(len(self.strategy)):
+            self.strategy[i] /= tmsum
+        self._fitness = None
+        self.ea = ea
+
+    @property
+    def fitness(self):
+        if self._fitness:
+            return self._fitness
+        else:
+            assert 0 <= self.genotype.args['Rf'] <= 1
+            assert 0 <= self.genotype.args['Lf'] <= 1
+            self._fitness = simulate_war(self, self.ea.pts, self.genotype.args['Rf'], self.genotype.args['Lf'])
+            return self._fitness
+
+    @property
+    def strategy_entropy(self):
+        tsum = 0
+        for p in self.strategy:
+            if p != 0:
+                tsum -= p * math.log(p, 2)
+        return tsum
+
+    def __repr__(self):
+        s = "<BlottoPhenotype("
+        s += ", ".join("%.2f"%x for x in self.strategy)
+        s += ")"
+        return s
