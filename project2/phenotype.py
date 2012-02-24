790dd8ed7b53e6694618b43dc6e77135e337114c
ernie <ernie@epicbytes.net>
2012-02-24 17:20:47 +0100

    added first sdm.. seems to work now

--- project2/phenotype.py
+++ project2/phenotype.py
@@ -3,14 +3,33 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 import main
 import genotype as geno
 import math
+import itertools
+import matplotlib.pyplot as plt
 
 from random import randint, random, shuffle
 
-def number_of_ones(array):
-    ones = 0
-    for i in [x for x in array if x == 1]:
-        ones += ones
-    return ones
+def read_training_data(number):
+    f = open('training_data/izzy-train'+number+'.dat', 'r')
+    training_data = [x for x in f.read().split(" ")]
+    f.close()
+    return training_data
+
+def find_spikes(data, threshold):
+    spikes = []
+    for point in xrange(len(data)-5):
+        max = 0
+        for i in range(5):
+            if data[point+i] > max:
+                max = data[point+i]
+        if data[point+2] > threshold and data[point+2] == max:
+            spikes.append(point+2)
+    return spikes
+
+def d_st(T_a, T_b, p):
+    sigma = 0
+    for t_ai, t_bi in itertools.izip(T_a, T_b):
+        sigma += math.pow(math.fabs(t_ai - t_bi), p)
+    return (1/len(min(T_a,T_b,key=len)))*math.pow(sigma, (1/p))
 
 class Phenotype(object):
     __metaclass__ = ABCMeta
@@ -26,11 +45,11 @@ class IzhikevichPhenotype(Phenotype):
         assert len(genotype.vector) == 481
         self.genotype = genotype
         self.generation = generation
-        self.a = number_of_ones(self.genotype.vector[0:199])/1000
-        self.b = number_of_ones(self.genotype.vector[200:229])/100
-        self.c = -number_of_ones(self.genotype.vector[230:280])-30
-        self.d = number_of_ones(self.genotype.vector[281:380])/10
-        self.k = number_of_ones(self.genotype.vector[381:480])/100
+        self.a = sum(self.genotype.vector[0:199])/1000
+        self.b = sum(self.genotype.vector[200:229])/100
+        self.c = -sum(self.genotype.vector[230:280])-30
+        self.d = sum(self.genotype.vector[281:380])/10
+        self.k = sum(self.genotype.vector[381:480])/100
         self.tau = 10
         self.I = 10
 
@@ -50,7 +69,9 @@ class IzhikevichPhenotype(Phenotype):
 
     @property
     def fitness(self):
-        pass
+        T_a = find_spikes(self.spike_train, 0)
+        T_b = find_spikes(read_training_data('1'), 0)
+        return 1/d_st(T_a, T_b, 2)
 
     def __repr__(self):
         s = "<IzhikevichPhenotype("
