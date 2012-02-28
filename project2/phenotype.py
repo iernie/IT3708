5a32a7cfd75a970d785094e917f31beb28ea4863
ernie <ernie@epicbytes.net>
2012-02-28 13:42:24 +0100

    same as before, silly me

--- project2/phenotype.py
+++ project2/phenotype.py
@@ -61,7 +61,7 @@ def d_si(T_a, T_b, p):
         tsum += abs((ai-ap) - (bi-bp))**p
     tsum = tsum ** (1/p)
     if N > 1:
-        tsum += abs(len(T_a) - len(T_b))*1000/min(len(self.T_a),len(T_b))
+        tsum += abs(len(T_a) - len(T_b))*1000/min(len(T_a),len(T_b))
         tsum = tsum / (N-1)
     else:
         tsum += abs(len(T_a) - len(T_b))*1000
