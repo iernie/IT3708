029294f75473d2cc164bfc0395176ba7ffdbb806
ernie <ernie@epicbytes.net>
2012-04-19 15:14:06 +0200

    search done

--- project4/swarm-epuck/webswarm/robot.py
+++ project4/swarm-epuck/webswarm/robot.py
@@ -1,10 +1,11 @@
+import random
+
 class Robot:
     def __init__(self):
         self.behaviours = self.get_behaviours()
         self.max_speed = 1.0
         self.weights = [[-1.0, -0.8],[-1.0, -0.8],[-0.4,  0.4],[ 0.0,  0.0],[ 0.0,  0.0],[ 0.4, -0.4],[-0.6, -0.8],[-0.6, -0.8]]
         self.offset = [0.5*self.max_speed, 0.5*self.max_speed]
-        self.close_threshold = 0.5
         self.stagnation_threshold = 1.0
         self.push_threshold = 0.1
 
@@ -15,26 +16,25 @@ class Robot:
 
         @register
         def search(data):
-            speed = [0.0, 0.0]
-            for i in range(2):
-                for j in range(8):
-                    speed[i] += data[j] * self.weights[j][i]
+            # proximity sensors
+            sensors_left = sum(data[0][4:])
+            sensors_right = sum(data[0][:4])
+            left_speed = self.minmax(sensors_left + (sensors_right - sensors_left) * random.Random())
+            right_speed = self.minmax(sensors_right + (sensors_left - sensors_right) * random.Random())
 
-                speed[i] = self.offset[i] + (speed[i] * self.max_speed)
-                if speed[i] > self.max_speed:
-                    speed[i] = self.max_speed
-                elif speed[i] < -self.max_speed:
-                    speed[i] = -self.max_speed
+            if left_speed == right_speed && left_speed >= self.distance_threshold:
+                right_speed = self.max_speed
 
-            return speed
+            return [left_speed, right_speed]
 
         @register
         def retrieval(data):
             if (data[1][0],data[1][7]) < (self.push_threshold, self.push_threshold):
                 return [self.max_speed, self.max_speed]
             else:
-                sensors_right = sum(data[1][:4])
+                # light sensors
                 sensors_left = sum(data[1][4:])
+                sensors_right = sum(data[1][:4])
                 if (sensors_left - sensors_right) < 0:
                     return [self.max_speed * 0.7, self.max_speed * -0.3]
                 else:
@@ -51,15 +51,18 @@ class Robot:
         # search, retrieval, stagnation
         behaviour = "search"
 
-        if self.is_close_to_object(data[0]):
+        if self.is_close_to_object(data[1]):
             behaviour = "retrieval"
         elif False:
             behaviour = "stagnation"
 
         return self.behaviours[behaviour](data)
 
+    def minmax(data, min_value, max_value):
+        return min(max(data,min_value),max_value)
+
     def is_close_to_object(self, data):
         for sensor in data:
-            if sensor > self.close_threshold:
+            if sensor > self.push_threshold:
                 return True
         return False
